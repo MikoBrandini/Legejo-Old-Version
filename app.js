04c2b05ce8a83cbdc59460bc3f4bcc602c1f6c5f
Miko Brandini <MikoBrandini@users.noreply.github.com>
2016-11-25 23:39:20 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,40 @@
+var express = require("express");
+var bdPars = require('body-parser');
+var methodOverride = require('method-override');
+var app = express();
+var pgp = require('pg-promise')();
+var mustacheExpress = require('mustache-express');
+var request = require('request');
+var rp = require('request-promise');
+
+app.engine('html', mustacheExpress());
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+app.use(express.static(__dirname + "/public/"));
+app.use(methodOverride('_method')); //method override
+app.use(bdPars.urlencoded({ extended: false })); //body parser
+app.use(bdPars.json()); //body parser
+
+
+
+var port = process.env.PORT || 8015;
+app.listen(port)
+console.log("Callooh! Callay! Server is running on " + port);
+
+
+
+app.get('/', function (req, res) {
+request('https://eo.wikipedia.org/api/rest_v1/page/mobile-sections/hindio', function (error, response, body) {
+    if(error){
+        return console.log('Error:', error);
+    }
+    if(response.statusCode !== 200){
+        return console.log('Invalid Status Code Returned:', response.statusCode);
+}
+
+})
+;})
+
+
+
+
