963ed83d9b9aa9992c820217a81911be56dea901
Miko Brandini <MikoBrandini@users.noreply.github.com>
2016-12-01 16:08:06 -0500

    added words into lists

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + "/public/"));
 app.use(methodOverride('_method')); //method override
 app.use(bdPars.urlencoded({
-    extended: false
+  extended: false
 })); //body parser
 app.use(bdPars.json()); //body parser
 
@@ -34,103 +34,95 @@ console.log("Callooh! Callay! Server is running on " + port);
 //log in and user creation stuff begins here
 
 app.use(session({
-    secret: 'theTruthIsOutThere51',
-    resave: false,
-    saveUninitialized: true,
-    cookie: {
-        secure: false
-    }
+  secret: 'theTruthIsOutThere51',
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    secure: false
+  }
 }))
 
 app.get("/", function(req, res) {
-    var logged_in;
-    var email;
-    if (req.session.user) {
-     logged_in = true;
-     email = req.session.user.email;
-    }
-    var data = {
-        "logged_in": logged_in,
-        "email": email
-            //variables columns from table
-    }
-    res.render('index', data)
+  var logged_in;
+  var email;
+  if (req.session.user) {
+    logged_in = true;
+    email = req.session.user.email;
+  }
+  var data = {
+    "logged_in": logged_in,
+    "email": email
+      //variables columns from table
+  }
+  res.render('index', data)
     //this sends over data, even if data is empy due to incorrect log in values
 })
 
 
 
-
-
-
 app.get("/signup", function(req, res) {
-    res.render('signup/index')
+  res.render('signup/index')
 })
 
 
 //to create a new user
 app.post("/signup", function(req, res) {
-    //save user to the database
-    //ni bezonas savi uzanton kaj poste meti la kasxvorton en la datumbazo
-    var data = req.body
+  //save user to the database
+  //ni bezonas savi uzanton kaj poste meti la kasxvorton en la datumbazo
+  var data = req.body
 
-    //bcrypt needs to use callbacks instead of promises, unfortunately.
-    bcrypt.hash(data.password, 10, function(err, hash) {
+  //bcrypt needs to use callbacks instead of promises, unfortunately.
+  bcrypt.hash(data.password, 10, function(err, hash) {
 
-        db.none(
-            "INSERT INTO users (name, email, password_digest) VALUES ($1, $2, $3)", [data.name, data.email, hash]
-        ).then(function() {
-            console.log("user created")
-                res.render('index')
-        })
+    db.none(
+      "INSERT INTO users (name, email, password_digest) VALUES ($1, $2, $3)", [data.name, data.email, hash]
+    ).then(function() {
+      console.log("user created")
+      res.render('index')
     })
+  })
 })
-
-
 
 
 
 //from the get thing up there, it redirects me up here.
 app.post('/login', function(req, res) {
   //there is no actual /login that's accessible to the client.
-    //we need to grab stuff off the body
-    //grabing from forms
-    var data = req.body;
-    //check if user exists is step one.
-    db.one(
-        "SELECT * FROM users WHERE email=$1", [data.email]
-        //the above in brackets sanatizes
-    ).catch(function() {
+  //we need to grab stuff off the body
+  //grabing from forms
+  var data = req.body;
+  //check if user exists is step one.
+  db.one(
+    "SELECT * FROM users WHERE email=$1", [data.email]
+    //the above in brackets sanatizes
+  ).catch(function() {
+    res.send('Email/Password not found')
+    console.log('Email/Password not found')
+      //we are ambigious above to make it easier. but it should just saw user.
+  }).then(function(user) {
+    //the order of the values in the comparison below matter.the first one must be the plaintext pw
+    bcrypt.compare(data.password, user.password_digest, function(err, cmp) {
+      if (cmp) {
+        req.session.user = user;
+        //sort of like a variable, it contains the data user, the row of the user database
+        //like a particular row in a user database
+        console.log("cmp succesful, email matches pw")
+        res.redirect('/');
+        //i need to be sent to the index page and just show the content meant for loggedin users
+      } else {
         res.send('Email/Password not found')
-        console.log('Email/Password not found')
-            //we are ambigious above to make it easier. but it should just saw user.
-    }).then(function(user) {
-        //the order of the values in the comparison below matter.the first one must be the plaintext pw
-        bcrypt.compare(data.password, user.password_digest, function(err, cmp) {
-            if (cmp) {
-                req.session.user = user;
-                //sort of like a variable, it contains the data user, the row of the user database
-                //like a particular row in a user database
-                console.log("cmp succesful, email matches pw")
-                    res.redirect('/');
-                    //i need to be sent to the index page and just show the content meant for loggedin users
-            } else{
-              res.send('Email/Password not found')
-            }
-
-        })
-
-
+      }
 
     })
+
+
+
+  })
 })
 
 
 
 //end of sign up and log in stuff
-
-
-
 
 
 
@@ -140,45 +132,41 @@ app.post("/addWord", function(req, res) {
     //save user to the database
     //ni bezonas savi uzanton kaj poste meti la kasxvorton en la datumbazo
     var data = req.body
-    //uncomment out the console log below later
-    // console.log(data)
-     // var userID=
-     // console.log(userID);
-        db.none(
-            "INSERT INTO lists (word, definition) VALUES ($1, $2)", [data.word, data.definition]
-        )
-        .catch(function(){
-          console.log("word not added, error.")
-        })
-        .then(function() {
-            console.log("word added")
-                res.render('wiki/articleTemplate')
-        })
-    })
-//
-//end of saving a new word
+    db.none(
+        "INSERT INTO lists (word, definition, user_id) VALUES ($1, $2, $3)", [data.word, data.definition, ]
+      )
+      .catch(function() {
+        console.log("word not added, error.")
+      })
+      .then(function() {
+        console.log("word added")
+        res.render('wiki/articleTemplate')
+      })
+  })
+  //
+  //end of saving a new word
 
 
 //render index page
 app.get('/', function(req, res) {
-    res.render('index');
+  res.render('index');
 });
 
 
 
 app.get('/wiki/template', function(req, res) {
-    request('https://eo.wikipedia.org/api/rest_v1/page/mobile-sections/francio', function(error, response, body) {
-        if (error) {
-            return console.log('Error:', error);
-        }
-        if (response.statusCode !== 200) {
-            return console.log('Invalid Status Code Returned:', response.statusCode);
-        }
-        var parsingThing = JSON.parse(body);
-        res.render('wiki/articleTemplate', {
-            "wikiData": parsingThing
-        })
+  request('https://eo.wikipedia.org/api/rest_v1/page/mobile-sections/francio', function(error, response, body) {
+    if (error) {
+      return console.log('Error:', error);
+    }
+    if (response.statusCode !== 200) {
+      return console.log('Invalid Status Code Returned:', response.statusCode);
+    }
+    var parsingThing = JSON.parse(body);
+    res.render('wiki/articleTemplate', {
+      "wikiData": parsingThing
     })
+  })
 })
 
 
--- public/js/script.js
+++ public/js/script.js
@@ -1,102 +1,94 @@
-  $( ".textHolders" ).each(function( index ) {
-  referencePoint=$(this)
-  var text=$(this).text()
-  var words=text.split(' ')
- $(this).text(' ')
- words.forEach(function(word){
-  return $(referencePoint).append("<span class = 'wikipediaArticleText'>"+word+ " " +"</span>") })
+// This is just a sample script. Paste your real code (javascript or HTML) here.
+$(".textHolders").each(function(index) {
+  referencePoint = $(this)
+  var text = $(this).text()
+  var words = text.split(' ')
+  $(this).text(' ')
+  words.forEach(function(word) {
+    return $(referencePoint).append("<span class = 'wikipediaArticleText'>" + word + " " + "</span>")
+  })
 });
 
 
 //get definition
 $('.wikipediaArticleText').click(function(event) {
- vorto=$(this).text()
- // console.log($(this).text())
-             $('#popUpContainer').hide()
-             $('#popUpTemplate').empty()
-             // console.log(this)
- // $(this).append('<div id=popUpTemplate></div>')
-    $.ajax({
-      "url": "http://www.simplavortaro.org/api/v1/trovi/"+vorto,
-      "method": "GET",
-      "success": function(data){
-        console.log('ajax call was good.')
-        $('#popUpContainer').show()
-        $('#popUpContainer').css('position', 'fixed');
-        // console.log(data)
-        // console.log("this is something "+data.preciza )
-        //second level ajax:
-        $.ajax({
-          "url": "http://www.simplavortaro.org/api/v1/vorto/"+data.preciza,
-          "method": "GET",
-          "success": function(dataB){
-            console.log(dataB)
+  vorto = $(this).text()
+    // console.log($(this).text())
+  $('#popUpContainer').hide()
+  $('#popUpTemplate').empty()
+    // console.log(this)
+    // $(this).append('<div id=popUpTemplate></div>')
+  $.ajax({
+    "url": "http://www.simplavortaro.org/api/v1/trovi/" + vorto,
+    "method": "GET",
+    "success": function(data) {
+      console.log('ajax call was good.')
+      $('#popUpContainer').show()
+      $('#popUpContainer').css('position', 'fixed');
+      // console.log(data)
+      // console.log("this is something "+data.preciza )
+      //second level ajax:
+      $.ajax({
+        "url": "http://www.simplavortaro.org/api/v1/vorto/" + data.preciza,
+        "method": "GET",
+        "success": function(dataB) {
+          console.log(dataB)
             //set up save button
             //define listener here to grab dataB
-      $('#popUpTemplate').append('<h1 id="popUpHeader">'+dataB.vorto+'</h1>' )
-     dataB.difinoj.forEach(function(x){return    $('#popUpTemplate').append('<p class="popUpText">'+x.difino+'</p>' )})
-          }
-        })
+          $('#popUpTemplate').append('<h1 id="popUpHeader">' + dataB.vorto + '</h1>')
+          dataB.difinoj.forEach(function(x) {
+            return $('#popUpTemplate').append('<p class="popUpText">' + x.difino + '</p>')
+          })
+        }
+      })
 
 
 
+      //this is the save word button
 
 
+      //this close code is taken from: http://stackoverflow.com/questions/22052317/hide-parent-div-on-click-using-jquery
+      $('.close').on("click", function() {
+        $(this).parents('div').hide();
+      });
 
 
+    }
+  })
+});
 
 
-//this is the save word button
 $('#saviorTheButton').click(function(event) {
+  event.preventDefault();
+  var definitionFromDiv = $('.popUpText').each(function(x) {
+    return this.innerHTML
+  })
 
-    var definitionFromDiv = $('.popUpText').each(function(x) {
-        return this.innerHTML
-    })
+  //empty object
+  var savedStuff = {};
+  savedStuff["word"] = $('#popUpHeader').html();
+  savedStuff['definition'] = definitionFromDiv.html();
 
-    //empty object
-    var savedStuff = {};
+  postData(savedStuff);
 
-    savedStuff["word"] = $('#popUpHeader').html();
-    savedStuff['definition'] = definitionFromDiv.html();
-
-    postData(savedStuff);
-
-             $('#popUpContainer').hide()
-             $('#popUpTemplate').empty()
+  $('#popUpContainer').hide()
+  $('#popUpTemplate').empty()
 })
 
 var postData = function(savedStuff) {
-    $.ajax({
-            url: 'http://localhost:8015/addWord',
-            type: 'POST',
-            data: savedStuff
-        })
-        .done(function() {
-            console.log("word saved");
-            //don't forget to clear the contents of the variables above (word, and definition)
-        })
-        .fail(function() {
-            console.log("error");
-        })
-        .always(function() {
-            console.log("complete");
-        });
-};
-
-
-//this close code is taken from: http://stackoverflow.com/questions/22052317/hide-parent-div-on-click-using-jquery
-$('.close').on("click", function () {
-    $(this).parents('div').hide();
-});
-
-
-
-
-
-
-
-        // console.log(typeof(data))
-        // console.log(JSON.stringify(data))
-      }
+  console.log('firing postData. savedStuff:');
+  $.ajax({
+      url: 'http://localhost:8015/addWord',
+      type: 'POST',
+      data: savedStuff
     })
-});
+    .done(function() {
+      console.log("word added to database");
+    })
+    .fail(function() {
+      console.log("error");
+    })
+    .always(function() {
+      console.log(".complete, completed with adding word to database");
+    });
+};
