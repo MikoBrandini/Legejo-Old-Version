748f2f8502a96aea03957e29e6d3e33ee00af16a
Miko Brandini <MikoBrandini@gmail.com>
2017-06-23 20:45:56 -0400

    reverted back to the original state of things

--- app.js
+++ app.js
@@ -43,8 +43,7 @@ app.use(session({
   }
 }))
 
-app.use(function(req, res, next) { res.header('Access-Control-Allow-Origin', "*"); res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE'); res.header('Access-Control-Allow-Headers', 'Content-Type'); next();
-})
+
 
 //this renders the front page
 app.get("/", function(req, res) {
